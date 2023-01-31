c660ba00458b3c2974921d56da30b8153f7c8b88
Åsa Björklund <asa.bjorklund@scilifelab.se>
2023-01-31 08:23:19 +0100

    Filtering in seurat object

---
+++ filter_cutoffs.R
@@ -0,0 +1,44 @@
+# filter cells in seurat object
+@ cells grouped by batch.key
+@ qc stat in meta.name meta.data slot.
+# define cutoff by median +/- mad.cut* mad 
+# upper TRUE/FALSE to cut upper/lower tail.
+
+filter_cutoffs = function(sobj, meta.name, batch.key = "orig.ident", 
+                          upper = TRUE, mad.cut = 2, 
+                          plot = T, with.points = FALSE){
+  d = sobj@meta.data[,meta.name]
+  names(d) = colnames(sobj)
+  per.batch = split(d, sobj@meta.data[,batch.key])
+  stats = lapply(per.batch, function(x){
+    m  = median(x)
+    s = mad(x)
+    if (upper){
+      cut = m + mad.cut*s
+      cells = names(x)[x < cut]
+    }else { 
+      cut = m - mad.cut*s
+      cells = which(x < cut)
+    }  
+    return(list(cells = cells, cutoff=cut))
+  })
+  keep.cells = unlist(lapply(stats, function(x) x$cells)  )
+  cutoffs = unlist(lapply(stats, function(x) x$cutoff)  )
+  print(cutoffs)
+  
+  if (plot){
+    meta = data.frame(batch = sobj@meta.data[,batch.key], data = d, cutoff = cutoffs[sobj@meta.data[,batch.key]])
+    p1 <- ggplot(meta, aes(x=batch, y=data, fill = batch)) + 
+      geom_violin(scale = "width", trim=TRUE, adjust = 1) + 
+      geom_errorbar(width=0.8, aes(ymax=cutoff, ymin=cutoff)) +
+      theme_classic() + 
+      RotatedAxis() + NoLegend() + 
+      ggtitle(meta.name)
+    if (with.points){ 
+      p1 = p1 + geom_jitter(height = 0, size = .1) + theme_classic()
+    }
+    print(p1)
+  }
+  
+  return(keep.cells)
+}
